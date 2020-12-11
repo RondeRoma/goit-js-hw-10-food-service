63f064259840a4acb97dccb664ec35126080335c
Roman <4895476@gmail.com>
2020-12-11 12:10:10 +0200

    рефакторинг

--- src/js/theme.js
+++ src/js/theme.js
@@ -16,15 +16,12 @@ function onChangeTheme(event) {
   if (check) {
     refs.body.classList.add(Theme.DARK);
     refs.body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
   } else {
     refs.body.classList.add(Theme.LIGHT);
     refs.body.classList.remove(Theme.DARK);
-  }
-  if (check) {
-    localStorage.setItem('theme', Theme.DARK);
-  } else {
     localStorage.setItem('theme', Theme.LIGHT);
-  }
+    }
 }
 
 const themeLocal = localStorage.getItem('theme');
